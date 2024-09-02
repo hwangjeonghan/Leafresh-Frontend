c258b446774b02108d1be40ff3baa3d718db1f2d
jinny <eujin.mstory@gmail.com>
2024-09-02 18:01:10 +0900

    feat: add main_dairy component

---
+++ src/components/diary/diaryCard.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+
+    여기는 feed 용 카드들이 반복될 거에요!!!!!!!!!
+
+</template>
---
+++ src/components/diary/diaryProfile.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+
+여기는 프로필 입니다! !! !! !
+
+</template>
