1771dfaa5088ae806c241e884b7a184fcc0563a9
ppudding3861 <ppuding3861@gmail.com>
2024-09-09 18:05:38 +0900

    feat : 프로필 페이지 등록

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import FeedAdd from "@/views/FeedAdded.vue"; // 피드 추가 페이지 import
 import MarketDetail from "@/views/MarketDetail.vue";
 import FeedDetail from "@/views/FeedDetail.vue";
 import Chat from '@/views/Chat.vue';
+import ProfileRegistration from '@/views/ProfileRegistration.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -69,6 +70,11 @@ const router = createRouter({
       name: 'Chat',
       component: Chat,
       props: true, // 경로 파라미터를 props로 컴포넌트에 전달
+    },
+    { 
+      path: '/profile-registration', 
+      name: 'ProfileRegistration',  
+      component: ProfileRegistration 
     },
   ],
 });
