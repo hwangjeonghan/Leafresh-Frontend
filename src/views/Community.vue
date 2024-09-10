6dc8ac2d95df7c18faa9d29468fb47ebfca2fbe1
Stjoo0925 <stjoo0925@gmail.com>
2024-09-10 17:51:18 +0900

    fix : fix components

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -14,7 +14,7 @@
 .commuity_container {
   width: 100%;
   height: 100%;
-  background-color: #f2d9bb;
+  background-color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
