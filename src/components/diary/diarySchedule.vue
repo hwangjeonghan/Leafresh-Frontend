96bb061fac6c1cc1df2fd4bf313e5a127ee468cf
jinny <eujin.mstory@gmail.com>
2024-09-12 12:39:03 +0900

    fix: diaryTodo

--- src/components/diary/diarySchedule.vue
+++ src/components/diary/diarySchedule.vue
@@ -1,22 +1,75 @@
 <script setup>
 import Calender from "./calender.vue";
-import Todo from "./todo.vue";
-import TodoForm from "./todoform.vue";
 import TodayTodo from "./todayTodo.vue";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import axios from 'axios';
+
 import {
-  todos,
   isModalOpen,
   newTodoContent,
   openModal,
   closeModal,
-  addTodo,
 } from "@/assets/js/todo"; // todo.js에서 가져옴
-import { computed } from "vue";
+import { ref, computed } from "vue";
+
+
 import { useTodoStore } from "@/stores/todoStore";
+import { useUserstore } from "@/stores/users";
+
 
 const todoStore = useTodoStore();
-// Pinia 스토어에서 클릭된 날짜 가져오기
-const clickedDate = computed(() => todoStore.clickedDate);
+const userStore = useUserstore();
+const userId = computed(() => userStore.userId);
+
+
+// API_BASE_URL 설정
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // VITE 환경변수로부터 읽어옴
+
+// 할 일 추가 함수
+const addTodo = async () => {
+  if (!newTodoContent.value || !date.value) {
+    alert('할 일 내용과 날짜를 모두 입력하세요.');
+    return;
+  }
+
+  try {
+    // 서버에 todo 데이터 전송
+    const response = await axios.post(`${API_BASE_URL}/garden-diary/todo/add`, {
+      todoContent: newTodoContent.value,  // 할 일 내용
+      todoSelectedDate: date.value,     // 선택된 날짜
+      userId: userId.value,           
+    });
+
+    if (response.status === 200) {
+      // 성공적으로 저장된 경우 Pinia 스토어에 추가
+      todoStore.addTodo({
+        content: newTodoContent.value,
+        selectedDate: date.value,
+        userId: userId.value, // 추가로 userId도 저장
+      });
+      alert("할 일이 성공적으로 저장되었습니다.");
+      
+        // 입력 필드 초기화
+        newTodoContent.value = '';
+        date.value = null;
+
+
+    } else {
+      alert("저장 중 오류가 발생했습니다.");
+    }
+  } catch (error) {
+    console.error(error);
+    alert("서버 요청 중 오류가 발생했습니다.");
+  }
+
+  closeModal();  // 모달 닫기
+};
+
+
+
+const date = ref();
+
 </script>
 
 <template>
@@ -37,12 +90,17 @@ const clickedDate = computed(() => todoStore.clickedDate);
 
         <button @click="openModal">할일추가</button>
       </div>
+
     </div>
 
     <div v-if="isModalOpen" class="modal-overlay" @click="closeModal">
       <div class="modal-content" @click.stop>
         <h2>Add New Todo</h2>
-        <TodoForm />
+
+        <div>
+          <h1>todo날짜를 입력하세용</h1>
+          <VueDatePicker v-model="date" />
+        </div>
         <input
           v-model="newTodoContent"
           placeholder="New todo"
